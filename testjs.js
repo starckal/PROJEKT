6bc327974b077dff4d3ffe97c5092dca523384de
hallchri <hallchri@arcada.fi>
2019-09-20 21:14:31 +0300

    slideToggle() funkar nu på alla enskilda classerna + fixat textDivsen till mitten

--- testjs.js
+++ testjs.js
@@ -3,8 +3,8 @@
 var programmen = dataJSON.Program;
 var i;
 for(var i = 0; i < dataJSON.Personal.length; i++) {
-  var lararna = lararna + "<p><button>" + " " +
-dataJSON.Personal[i].Förnamn + " " + dataJSON.Personal[i].Efternamn+"</button><p id='moro'>MOJDÅ JA FÖRSVINNER</p></p>";   //Loopar igenom alla förnamn och efternamn och
+  var lararna = lararna + "<p><button class='slide-button'>" + " " +
+dataJSON.Personal[i].Förnamn + " " + dataJSON.Personal[i].Efternamn+"</button><p class='moro'>MOJDÅ JA FÖRSVINNER</p></p>";   //Loopar igenom alla förnamn och efternamn och
                                                                                                                           //sätter dem i en button
 
 }
