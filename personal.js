8b8bd6278f48a83a84f5e86eab343ac58d758969
unknown <hallchri@arcada.fi>
2019-09-30 22:32:53 +0300

    Fixade <select> å <option> på all personal

---
+++ personal.js
@@ -0,0 +1,56 @@
+jsonPersonal = {
+    "Personal": [
+        {
+          "Förnamn": "Dennis",
+          "Efternamn": "Biström"
+        },
+        {
+          "Förnamn": "Magnus",
+          "Efternamn": "Westerlund"
+        },
+        {
+          "Förnamn": "Jonny",
+          "Efternamn": "Karlsson"
+        },
+        {
+          "Förnamn": "Dan",
+          "Efternamn": "Granqvist"
+        },
+        {
+          "Förnamn": "Richard",
+          "Efternamn": "Weber"
+        },
+        {
+          "Förnamn": "Pekka",
+          "Efternamn": "Butler"
+        },
+        {
+          "Förnamn": "Robin",
+          "Efternamn": "Wikström"
+        },
+        {
+          "Förnamn": "Kaj-Mikael",
+          "Efternamn": "Björk"
+        },
+        {
+          "Förnamn": "Joachim",
+          "Efternamn": "Enkvist"
+        },
+        {
+          "Förnamn": "Andrey",
+          "Efternamn": "Shcherbakov"
+        },
+        {
+          "Förnamn": "Leonardo",
+          "Efternamn": "Espinosa Leal"
+        },
+        {
+          "Förnamn": "Anton",
+          "Efternamn": "Akusok"
+        },
+        {
+          "Förnamn": "Shuhua",
+          "Efternamn": "Liu"
+        }
+      ]
+}
