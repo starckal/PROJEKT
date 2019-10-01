9a9f8e2a8c5fd79ca22d17e8a17fe7677d948996
unknown <hallchri@arcada.fi>
2019-10-01 19:44:59 +0300

    test

--- testjs.js
+++ testjs.js
@@ -1,17 +1,14 @@
-
-
 var programmen = dataJSON.Program;
 var lararna = '';
 for(var i = 0; i < dataJSON.Personal.length; i++) {
     lararna = lararna + "<p><button class='slide-button'>" + " " +
     dataJSON.Personal[i].Förnamn + " " + dataJSON.Personal[i].Efternamn+"</button><p class='moro'>MOJDÅ JA FÖRSVINNER</p></p>";   //Loopar igenom alla förnamn och efternamn och
-                                                                                                                          //sätter dem i en button
-
+                                                                                                                                  //sätter dem i en button
 }
 var kurs = '';
 for(var i = 0; i < programmen.IA.Kurser.length; i++) { // Skriver ut kurserna på likadant sätt som lärarna skrivs ut
     kurs = kurs + "<p><button class='slide-button'>" + programmen.IA.Kurser[i].namn +"</button><p class='moro'>"+ "Kurskod: " + programmen.IA.Kurser[i].kurskod + "<br>" 
-  + "Period: " + programmen.IA.Kurser[i].period + "<br>" + "Timmar: " +  programmen.IA.Kurser[i].timmar +"</p></p>";
+  + "Period: " + programmen.IA.Kurser[i].period + "<br>" + "Timmar: " +  programmen.IA.Kurser[i].timmar + "</p></p>";
 
 
   //console.log(kurs); // miksi vitussa tämä on undefined saatana perkele | haha olet tyhäm kone ja vann dig
