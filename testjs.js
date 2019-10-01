28276e402396e9bd65c4c06b88add8dbc2dae625
Isak Svarvar <isak.svarvar@gmail.com>
2019-10-01 21:13:21 +0300

    updated testjs with if/else

--- testjs.js
+++ testjs.js
@@ -6,14 +6,27 @@ for(var i = 0; i < dataJSON.Personal.length; i++) {
                                                                                                                                   //sätter dem i en button
 }
 var kurs = '';
-for(var i = 0; i < programmen.IA.Kurser.length; i++) { // Skriver ut kurserna på likadant sätt som lärarna skrivs ut
-    kurs = kurs + "<p><button class='slide-button'>" + programmen.IA.Kurser[i].namn +"</button><p class='moro' style='display:none'>"+ "Kurskod: " + programmen.IA.Kurser[i].kurskod + "<br>" 
-  + "Period: " + programmen.IA.Kurser[i].period + "<br>" + "Timmar: " +  programmen.IA.Kurser[i].timmar + "</p></p>";
+if(sessionStorage.picker == "master"){
+	for(var i = 0; i < programmen.BDA.Kurser.length; i++) { // Skriver ut kurserna på likadant sätt som lärarna skrivs ut
+	    kurs = kurs + "<p><button class='slide-button'>" + programmen.BDA.Kurser[i].namn +"</button><p class='moro' style='display:none'>"+ "Kurskod: " + programmen.BDA.Kurser[i].kurskod + "<br>" 
+	  + "Period: " + programmen.BDA.Kurser[i].period + "<br>" + "Timmar: " +  programmen.BDA.Kurser[i].timmar + "</p></p>";
 
 
-  //console.log(kurs); // miksi vitussa tämä on undefined saatana perkele | haha olet tyhäm kone ja vann dig
+	  //console.log(kurs); // miksi vitussa tämä on undefined saatana perkele | haha olet tyhäm kone ja vann dig
 
+	}
+	
+} else {
+	for(var i = 0; i < programmen.IA.Kurser.length; i++) { // Skriver ut kurserna på likadant sätt som lärarna skrivs ut
+		    kurs = kurs + "<p><button class='slide-button'>" + programmen.IA.Kurser[i].namn +"</button><p class='moro' style='display:none'>"+ "Kurskod: " + programmen.IA.Kurser[i].kurskod + "<br>" 
+		  + "Period: " + programmen.IA.Kurser[i].period + "<br>" + "Timmar: " +  programmen.IA.Kurser[i].timmar + "</p></p>";
+
+
+		  //console.log(kurs); // miksi vitussa tämä on undefined saatana perkele | haha olet tyhäm kone ja vann dig
+
+		}
 }
+
 
  
 
