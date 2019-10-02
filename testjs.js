a375a3a8e85941987b8d280790473bd51395498d
Alex Starck <alexmikaelstarck@gmail.com>
2019-10-02 16:14:52 +0300

    ger unika id:n till p taggar for loop

--- testjs.js
+++ testjs.js
@@ -2,7 +2,7 @@ var programmen = dataJSON.Program;
 var lararna = '';
 for(var i = 0; i < dataJSON.Personal.length; i++) {
     lararna = lararna + "<p><button class='slide-button'>" + " " +
-    dataJSON.Personal[i].Förnamn + " " + dataJSON.Personal[i].Efternamn+"</button><p class='moro' style='display:none'>MOJDÅ JA FÖRSVINNER</p></p>";   //Loopar igenom alla förnamn och efternamn och
+    dataJSON.Personal[i].Förnamn + " " + dataJSON.Personal[i].Efternamn+"</button><p class='moro' id='koe-"+ i +"' style='display:none'>MOJDÅ JA FÖRSVINNER</p></p>";   //Loopar igenom alla förnamn och efternamn och
                                                                                                                                   //sätter dem i en button
 }
 var kurs = '';
